49e04a6a870da37705c73d100ff00706862b6b85
fellodurant <fellodurant97@gmail.com>
2019-03-19 09:52:10 +0300

    Created class for dialog window

--- src/main/java/com/felix/hospital/App.java
+++ src/main/java/com/felix/hospital/App.java
@@ -7,5 +7,16 @@ public class App {
         Query q1 = new Query("consult", new Term[]{new Atom("disease.pl")});
         
         System.out.println("Consult: " + (q1.hasSolution() ? "Sucess" : "Failed"));
+        
+        // Start system
+        Query q2 = new Query("go");
+        System.out.println((q2.hasSolution()));
+        
+        //Variable X = new Variable("Question");
+        //Query q3 = new Query("askQuestion", new Term[]{new Atom("Question")});
+        
+        Query q3 = new Query("hypothesis", new Term[]{new Atom("cold", "C")});
+        if(q3.hasSolution()) {
+        }
     }
 }
---
+++ src/main/java/com/felix/hospital/MainWindow.java
@@ -0,0 +1,16 @@
+package com.felix.hospital;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+
+public class MainWindow extends JFrame{
+    private JLabel title, date, question;
+    private JRadioButton answer;
+    private JButton cancel, next;
+    
+    public MainWindow() {
+        
+    }
+}
