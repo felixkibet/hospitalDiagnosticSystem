33f1fa75a7126bf194a6a0872baef442ef64a3bd
Adams Korir <adekorir@gmail.com>
2019-04-01 04:49:54 +0300

    Added improvements to the user interface.

--- src/main/java/com/felix/hospital/EntryPane.java
+++ src/main/java/com/felix/hospital/EntryPane.java
@@ -1,29 +1,29 @@
 package com.felix.hospital;
 
 import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.WindowEvent;
 import java.util.Arrays;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 class EntryPane extends JPanel {
 
-    private final JLabel lblQuestion, lblSuggestion;
+    private final JTextField fldSymptoms;
     private final JRadioButton rdoYes, rdoNo, rdoUnknown;
     private final JTextArea fldResults;
 
@@ -31,47 +31,61 @@ class EntryPane extends JPanel {
         super(new BorderLayout(10, 10));
 
         // initialize instance variables
-        lblQuestion     = new JLabel();
-        lblSuggestion   = new JLabel();
-        rdoNo           = new JRadioButton("No");
-        rdoYes          = new JRadioButton("Yes");
-        rdoUnknown      = new JRadioButton("Unspecified");
-        fldResults      = new JTextArea();
+        fldSymptoms = new JTextField();
+        rdoNo = new JRadioButton("No");
+        rdoYes = new JRadioButton("Yes");
+        rdoUnknown = new JRadioButton("Unspecified");
+        fldResults = new JTextArea();
+
+        // initialize everything
+        clear();
+
+        fldSymptoms.setEditable(false);
+        JButton btnNext = new JButton("NEXT");
+        btnNext.addActionListener(l -> {});
+        
+        // add the question and symptoms
+        JPanel top = new JPanel(new BorderLayout(10, 10));
+        top.setBorder(new TitledBorder("Consultation"));
+        top.add(new JLabel("Does the patient have the following symptoms?"), BorderLayout.NORTH);
+
+        // query panel [top] center layout
+        final JPanel qpCenter = new JPanel();
+        LayoutManager vertical = new BoxLayout(qpCenter, BoxLayout.PAGE_AXIS);
+        qpCenter.setLayout(vertical);
+        qpCenter.add(fldSymptoms);
+
+        // add rigid section
+        qpCenter.add(Box.createVerticalStrut(10));
 
         // add the radio buttons to the group and panel
         final ButtonGroup radioGroup = new ButtonGroup();
         final JPanel radioPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 10, 10));
-        for (JRadioButton rdoButton: Arrays.asList(rdoNo, rdoYes, rdoUnknown)) {
+        for (JRadioButton rdoButton : Arrays.asList(rdoNo, rdoYes, rdoUnknown)) {
             radioGroup.add(rdoButton);
             radioPanel.add(rdoButton);
         }
-        
-        final int ROWS = 2, COLS = 2;
-        JPanel form = new JPanel(new GridLayout(ROWS, COLS, 10, 15));
-        // populate the form
-        JComponent[][] components = {
-            {lblQuestion, lblSuggestion },
-            {new JLabel("Answer:"), radioPanel}
-        };
+        qpCenter.add(radioPanel);
+        qpCenter.add(qpCenter, BorderLayout.CENTER);
 
-        // add everything
-        for (JComponent[] row: components) {
-            for (JComponent comp: row) {
-                form.add(comp);
-            }
-        }
+        qpCenter.add(Box.createVerticalStrut(10));
 
-        fldResults.setEditable(false);
+        final JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        buttonPanel.add(btnNext);
+        qpCenter.add(buttonPanel);
+
+        // add a consult button
+        top.add(new JButton("CONSULT"), BorderLayout.EAST);
+
         fldResults.setSize(400, 250);
 
-        this.add(form, BorderLayout.NORTH);
+        this.add(qpCenter, BorderLayout.NORTH);
         this.add(fldResults, BorderLayout.CENTER);
     }
 
     private void clear() {
         // todo: clear the screen
-        lblQuestion.setText("");
-        lblSuggestion.setText("");
+        fldSymptoms.setText("");
         rdoNo.setSelected(true);
         fldResults.setText("");
     }
@@ -87,19 +101,20 @@ class EntryPane extends JPanel {
             final JPanel contentPane = new JPanel(new BorderLayout());
             contentPane.setBorder(new EmptyBorder(10, 10, 0, 10));
             window.setContentPane(contentPane);
-            
+
             // place instructions at the top
-            final JLabel lblInstructions = new JLabel("Please enter your queries below and they will be answered.", JLabel.LEFT);
+            final JLabel lblInstructions = new JLabel("Please enter your queries below and they will be answered.",
+                    JLabel.LEFT);
             contentPane.add(lblInstructions, BorderLayout.NORTH);
-            
+
             // add the entry pane
             final EntryPane pane = new EntryPane();
             contentPane.add(pane, BorderLayout.CENTER);
-            
+
             // add the close and clear buttons
             final JButton btnClose = new JButton("Close");
             btnClose.addActionListener(l -> window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING)));
-            
+
             final JButton btnClear = new JButton("Clear");
             btnClear.addActionListener(l -> pane.clear());
 
