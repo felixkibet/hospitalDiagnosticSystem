82568fdbbe41b32b04d1b52e81b96fcb31b9c10a
Adams Korir <adekorir@gmail.com>
2019-04-01 03:02:55 +0300

    Created user interface class and added some initial code.
    This will extend a JPanel and will have a form at the top followed by the response at the center.
    
    ~ Adams Korir

---
+++ src/main/java/com/felix/hospital/EntryPane.java
@@ -0,0 +1,12 @@
+package com.felix.hospital;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class Form extends JPanel {
+
+    public Form() {
+        super(new BorderLayout(10, 10));
+    }
+}
